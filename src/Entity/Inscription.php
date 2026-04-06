49d5ae0e2b7a22cb6e251d6458fe97849f064fed
hedi <hedi.cheikh@esprit.tn>
2026-04-06 20:59:59 +0100

    Ajout page packs et inscription pack

--- src/Entity/Inscription.php
+++ src/Entity/Inscription.php
@@ -2,11 +2,10 @@
 
 namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use App\Enum\StatutInscription;
 use App\Repository\InscriptionRepository;
+use App\Enum\StatutInscription;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: InscriptionRepository::class)]
 #[ORM\Table(name: 'inscription')]
@@ -14,82 +13,90 @@ class Inscription
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
+    #[ORM\Column(name: 'id_inscription', type: 'integer')]
     private ?int $id_inscription = null;
 
-    public function getId_inscription(): ?int
+    #[ORM\Column(name: 'date_inscription', type: 'datetime')]
+    private ?\DateTimeInterface $date_inscription = null;
+
+    #[Assert\NotBlank(message: 'Le statut est obligatoire.')]
+    #[ORM\Column(name: 'statut_inscr', type: 'string', enumType: StatutInscription::class, length: 255)]
+    private ?StatutInscription $statut_inscr = null;
+
+    #[ORM\Column(name: 'montant_total', type: 'decimal', precision: 10, scale: 2)]
+    private ?string $montant_total = null;
+
+    #[Assert\NotBlank(message: 'Le nom utilisateur est obligatoire.')]
+    #[Assert\Length(min: 3, max: 255)]
+    #[ORM\Column(name: 'nom_user', type: 'string', length: 255, nullable: true)]
+    private ?string $nom_user = null;
+
+    #[ORM\Column(name: 'nom_pack', type: 'string', length: 255, nullable: true)]
+    private ?string $nom_pack = null;
+
+    #[ORM\ManyToOne(targetEntity: Pack::class)]
+    #[ORM\JoinColumn(name: 'id_pack', referencedColumnName: 'id_pack', nullable: true)]
+    private ?Pack $pack = null;
+
+    public function getIdInscription(): ?int
     {
         return $this->id_inscription;
     }
 
-    public function setId_inscription(int $id_inscription): self
-    {
-        $this->id_inscription = $id_inscription;
-        return $this;
-    }
-
-    #[ORM\Column(type: 'datetime', nullable: false)]
-    private ?\DateTimeInterface $date_inscription = null;
-
-    public function getDate_inscription(): ?\DateTimeInterface
+    public function getDateInscription(): ?\DateTimeInterface
     {
         return $this->date_inscription;
     }
 
-    public function setDate_inscription(\DateTimeInterface $date_inscription): self
+    public function setDateInscription(\DateTimeInterface $date_inscription): self
     {
         $this->date_inscription = $date_inscription;
         return $this;
     }
 
-
-
-#[ORM\Column(enumType: StatutInscription::class)]
-private ?StatutInscription $statut_inscr = null;
-
-    public function getStatut_inscr(): ?StatutInscription
+    public function getStatutInscr(): ?StatutInscription
     {
         return $this->statut_inscr;
     }
 
-    public function setStatut_inscr(StatutInscription $statut_inscr): self
+    public function setStatutInscr(StatutInscription $statut_inscr): self
     {
         $this->statut_inscr = $statut_inscr;
         return $this;
     }
 
-#[ORM\Column(type: 'decimal', precision: 10, scale: 2)]
-private ?string $montant_total = null;
-
-    public function getMontant_total(): ?float
+    public function getMontantTotal(): ?string
     {
         return $this->montant_total;
     }
 
-    public function setMontant_total(float $montant_total): self
+    public function setMontantTotal(string $montant_total): self
     {
         $this->montant_total = $montant_total;
         return $this;
     }
 
-    #[ORM\ManyToOne(targetEntity: UserApp::class, inversedBy: 'inscriptions')]
-    #[ORM\JoinColumn(name: 'id_user', referencedColumnName: 'id_user')]
-    private ?UserApp $userApp = null;
-
-    public function getUserApp(): ?UserApp
+    public function getNomUser(): ?string
     {
-        return $this->userApp;
+        return $this->nom_user;
     }
 
-    public function setUserApp(?UserApp $userApp): self
+    public function setNomUser(?string $nom_user): self
     {
-        $this->userApp = $userApp;
+        $this->nom_user = $nom_user;
         return $this;
     }
 
-    #[ORM\ManyToOne(targetEntity: Pack::class, inversedBy: 'inscriptions')]
-    #[ORM\JoinColumn(name: 'id_pack', referencedColumnName: 'id_pack')]
-    private ?Pack $pack = null;
+    public function getNomPack(): ?string
+    {
+        return $this->nom_pack;
+    }
+
+    public function setNomPack(?string $nom_pack): self
+    {
+        $this->nom_pack = $nom_pack;
+        return $this;
+    }
 
     public function getPack(): ?Pack
     {
@@ -101,33 +108,4 @@ private ?string $montant_total = null;
         $this->pack = $pack;
         return $this;
     }
-
-#[ORM\Column(type: 'string', length: 255, nullable: true)]
-private ?string $nom_user = null;
-
-    public function getNom_user(): ?string
-    {
-        return $this->nom_user;
-    }
-
-    public function setNom_user(?string $nom_user): self
-    {
-        $this->nom_user = $nom_user;
-        return $this;
-    }
-
-#[ORM\Column(type: 'string', length: 255, nullable: true)]
-private ?string $nom_pack = null;
-
-    public function getNom_pack(): ?string
-    {
-        return $this->nom_pack;
-    }
-
-    public function setNom_pack(?string $nom_pack): self
-    {
-        $this->nom_pack = $nom_pack;
-        return $this;
-    }
-
 }
--- src/Repository/PackRepository.php
+++ src/Repository/PackRepository.php
@@ -6,9 +6,6 @@ use App\Entity\Pack;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Pack>
- */
 class PackRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
@@ -16,28 +13,83 @@ class PackRepository extends ServiceEntityRepository
         parent::__construct($registry, Pack::class);
     }
 
-//    /**
-//     * @return Pack[] Returns an array of Pack objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function findForFront(
+        ?string $search = null,
+        ?string $sort = null,
+        ?string $type = null,
+        ?string $statut = null
+    ): array {
+        $qb = $this->createQueryBuilder('p');
 
-//    public function findOneBySomeField($value): ?Pack
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+        if (!empty($search)) {
+            $qb->andWhere('LOWER(p.nom) LIKE :search OR LOWER(p.type_pack) LIKE :search')
+               ->setParameter('search', '%' . mb_strtolower(trim($search)) . '%');
+        }
+
+        if (!empty($type)) {
+            $qb->andWhere('LOWER(p.type_pack) = :type')
+               ->setParameter('type', mb_strtolower(trim($type)));
+        }
+
+        if (!empty($statut)) {
+            $qb->andWhere('LOWER(p.statut_pack) = :statut')
+               ->setParameter('statut', mb_strtolower(trim($statut)));
+        }
+
+        switch ($sort) {
+            case 'nom_desc':
+                $qb->orderBy('p.nom', 'DESC');
+                break;
+
+            case 'prix_asc':
+                $qb->orderBy('p.prix_base', 'ASC');
+                break;
+
+            case 'prix_desc':
+                $qb->orderBy('p.prix_base', 'DESC');
+                break;
+
+            case 'reduction_desc':
+                $qb->orderBy('p.reduction', 'DESC');
+                break;
+
+            case 'activites_desc':
+                $qb->orderBy('p.nb_activites_max', 'DESC');
+                break;
+
+            default:
+                $qb->orderBy('p.nom', 'ASC');
+                break;
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+
+    public function findDistinctTypes(): array
+    {
+        $rows = $this->createQueryBuilder('p')
+            ->select('DISTINCT p.type_pack AS type_pack')
+            ->where('p.type_pack IS NOT NULL')
+            ->andWhere('p.type_pack != :vide')
+            ->setParameter('vide', '')
+            ->orderBy('p.type_pack', 'ASC')
+            ->getQuery()
+            ->getArrayResult();
+
+        return array_map(fn ($row) => $row['type_pack'], $rows);
+    }
+
+    public function findDistinctStatuts(): array
+    {
+        $rows = $this->createQueryBuilder('p')
+            ->select('DISTINCT p.statut_pack AS statut_pack')
+            ->where('p.statut_pack IS NOT NULL')
+            ->andWhere('p.statut_pack != :vide')
+            ->setParameter('vide', '')
+            ->orderBy('p.statut_pack', 'ASC')
+            ->getQuery()
+            ->getArrayResult();
+
+        return array_map(fn ($row) => $row['statut_pack'], $rows);
+    }
 }
